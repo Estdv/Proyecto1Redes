9ca27833c77f09822a1ceb7d685978f69c45877b
Estdv <35667336+Estdv@users.noreply.github.com>
2021-08-07 21:27:07 -0600

    Update ChatXMPP.py
    implementacion de menu

--- ChatXMPP.py
+++ ChatXMPP.py
@@ -17,3 +17,53 @@ from slixmpp.xmlstream.stanzabase import ET, ElementBase
 import slixmpp
 import base64, time
 import threading
+
+print("BIENVENIDO AL CHAT")
+print("PRESIONE 1 PARA REGISTRARSE EN EL SERVIDOR DE ALUMCHAT")
+print("PRESIONE 2 PARA REGISTRARSE EN EL SERVIDOR DE ALUMCHAT")
+print("PRESIONE 3 PARA SALIR")
+
+op = input("")
+
+while (op != "3"):
+    if(op== "1"):
+        #login
+
+    elif(op == "2"):
+        #Register
+
+    else:
+        print("Opcion invalida intente denuevo")
+
+    while(op2 != "9"):
+
+        if(op2 =="1"):
+            #mostrar contactos
+
+        elif(op2 == "2"):
+            #Agregar contacto
+
+        elif(op2 == "3"):
+            #MOstrar detalles de un contacto
+
+        elif(op2 == "4"):
+            #Chat 1 a 1
+
+        elif(op2 == "5"):
+            #Chat grupal
+
+        elif(op2 == "6"):
+            #Def mensaje de presencia
+
+        elif(op2 == "7"):
+            #Eliminar cuenta
+
+        elif(op2 == "8"):
+            op2 = "9"
+            op = "3"
+
+        else:
+            print("Opcion invalida intente denuevo")
+
+print("GRACIAS HASTA LUEGO")
+        
